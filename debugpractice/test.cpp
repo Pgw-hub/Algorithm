e1d4fc574175b396d86b41c98d4be5e1cbf59bde
parkgeonwoo <80042706+Pgw-hub@users.noreply.github.com>
2022-03-16 15:56:18 +0900

    change directoryname & debugpractice_uncompleted

---
+++ debugpractice/test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *args[]){
+    cout << "input numbers: " << endl;
+    int x;
+    cin >> x;
+    cout << "hello : " << x << endl;
+    return 0;
+}
