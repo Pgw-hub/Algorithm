20cd6ddea1215123a00e46d662bc20f0b5e669d0
parkgeonwoo <80042706+Pgw-hub@users.noreply.github.com>
2022-03-18 17:57:18 +0900

    delete error version

--- hw/hw1/priorityqueue.cpp
+++ hw/hw1/priorityqueue.cpp
@@ -25,6 +25,7 @@ struct Heap {
 void grow(Heap* h,string key, float value);
 void trim(Heap* h);
 void printall(Heap* h);
+void change(Heap* h);
 void swim(Heap* h,int index);
 void sink(Heap* h,int index);
 void swap(Heap* h,int i,int j);
@@ -61,6 +62,7 @@ int main(){
                 trim(h);
                 break;
             case 'C' :
+                change(h);
                 break;
             case 'P' :
                 printall(h);
@@ -81,16 +83,40 @@ int main(){
 void grow(Heap* h,string key, float value){
     // h -> nodes[h->N].name = key;
     // h -> nodes[h->N].score = value;
-    h -> N++; 
+    h -> N++;
     h -> nodes[h->N] = {key, value};
     cout << "New element [" << h->nodes[h->N].name << "," << h->nodes[h->N].score << "]" << " is inserted." << endl << endl;
+    
 }
 
 void trim(Heap* h){
     cout << "[" << h->nodes[1].name << "," << h->nodes[1].score << "]" << " is deleted." << endl << endl;
-    swap(h,h->N,1);
+    swap(h,1,h->N);
     h -> N--;
     sink(h,1);
+}
+
+void change(Heap* h){
+    int key;
+    float new_score;
+
+    while(1){
+        cout << "Enter indx of element :" << endl;
+        cin >> key;
+        cout << "Enter new key value :" << endl;
+        cin >> new_score;
+
+        key = key + 1;//원래 index보다 1씩 큼.
+
+        if(new_score > h -> nodes[key].score){
+            cout << "new key value is higher than original one." << endl << "Enter proper input" << endl;
+        }
+        else{
+            h->nodes[key].score = new_score;
+             swim(h,key);
+             break;
+        }
+    }
 }
 
 void printall(Heap* h){
@@ -103,14 +129,16 @@ void printall(Heap* h){
 void swim(Heap* h,int index){
     //부모가 자식보다 크면 바꿈. 
     //부모는 현재 인덱스에서 /2한것임.
-    int c_i = index;
-    int p_i = c_i/2;
-    if(h->N >1)
-    while(h->nodes[p_i].score > h->nodes[c_i].score && p_i >0){
+//    if(index <= 2) return;
+//    int c_i = index;
+//    int p_i = c_i/2;
+//   I if(h->N >1)
+    while(h->nodes[index/2].score > h->nodes[index].score && index >1){
         //바꾸고, 그 바꾼 친구의 조부모와 또 비교해야함. 그럼 인덱스를 
-        swap(h,p_i,c_i);
-        c_i = p_i;//스왑되면 부모를 자식으로 만들고, 
-        p_i = c_i/2;// 조부모님을 결정해줌.
+        swap(h,index/2,index);
+        index = index/2;
+//        c_i = p_i;//스왑되면 부모를 자식으로 만들고,
+//        p_i = c_i/2;// 조부모님을 결정해줌.
     }
 }
 
@@ -126,14 +154,28 @@ void sink(Heap* h,int index){
     int check = 0;
 
     while( p_i * 2 <= h -> N){
-        if(h->nodes[p_i].score > h->nodes[lci].score){
-            swap(h,p_i,lci);
-            p_i = lci;
-            check = 1;
-        }
-        if(h->nodes[p_i].score < h->nodes[rci].score){
-            swap(h,p_i,rci);
-            p_i = rci;
+        int sci = 0;
+        // if(h->nodes[p_i].score > h->nodes[lci].score){
+        //     swap(h,p_i,lci);
+        //     p_i = lci;
+        //     check = 1;
+        // }
+        // if(h->nodes[p_i].score < h->nodes[rci].score){
+        //     swap(h,p_i,rci);
+        //     p_i = rci;
+        //     check = 1;
+        // }
+        //lci랑 rci를 비교해서 더 작은 친구와 바꿈.
+          //먼저 lci랑 rci중 더 작은 친구를 구하기.
+          //그리고 부모가 더 작은 친구보다 작다면,바꾸기.
+          //작지않다면 반복문 탈출.
+
+        if(h->nodes[lci].score > h-> nodes[rci].score) sci = rci;
+        else sci = lci;
+
+        if(h->nodes[p_i].score > h->nodes[sci].score){
+            swap(h,p_i,sci);
+            p_i = sci;
             check = 1;
         }
         if(check == 1){
