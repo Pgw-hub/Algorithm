0accfd5a895051cbedfd9f2764a02a37f30257b1
parkgeonwoo <80042706+Pgw-hub@users.noreply.github.com>
2022-05-02 19:02:36 +0900

    complete binary change and debeg problem

--- baekjoon/basic/binarychange.cpp
+++ baekjoon/basic/binarychange.cpp
@@ -3,24 +3,38 @@
 
 using namespace std;
 
-void modifier(char element);
+void modifier(char element,int i);
 
 int main(){
 	string num;
 	cin >> num;
 	for(int i=0; i < num.size(); i++){
-		modifier(num[i]);
+		modifier(num[i],i);
 	}
 }
 
-void modifier(char element){
+void modifier(char element, int i){
 	stack<int> s;
 	int a = element - '0';
+	int temp = a;
 
 	while(a){
 		s.push(a % 2);
 		a = a / 2;
 	}
+
+	if(i != 0){
+		if(temp < 4) s.push(0);
+		if(temp < 2) s.push(0); 
+		if(temp == 0) s.push(0);
+	}
+	else{
+		if(temp == 0){
+			cout << '0';
+			return;
+		}
+	}
+	
 	while(!s.empty()){
 		cout << s.top();
 		s.pop();
