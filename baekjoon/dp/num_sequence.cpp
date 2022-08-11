9f9b1e83a3a035531fbb7e5f6a83cd936b197dfb
parkgeonwoo <80042706+Pgw-hub@users.noreply.github.com>
2022-08-11 17:07:10 +0900

    solve conflict

---
+++ baekjoon/dp/num_sequence.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main (){
+
+	int N;
+	int tmp;
+	vector<int> v;
+
+	cin >> N;
+
+	for(int i = 0; i < N; i++){
+		cin >> tmp;
+		v.push_back(tmp);
+	}
+
+	
+
+
+
+return 0;
+}
--- baekjoon/dp/stair.cpp
+++ baekjoon/dp/stair.cpp
@@ -28,9 +28,16 @@ int main(){
 	mem[0] = 0;
 	mem[1] = v[1];
 	mem[2] = v[1] + v[2];
+<<<<<<< HEAD
 	//도착지를 기준으로 두칸전 vs 세칸전의 max + 한칸전을 비교.
 	for(int i = 3; i <= size; i++){
 		mem[i] = max(mem[i-2], mem[i-3] + v[i-1]) + v[i];
+=======
+	//3번째 칸부터는 연속으로 3번밟을 수 없으니, 처음에 두칸뛰고 밟을지, 한칸밟고 생각하고 가야함.
+
+	for(int i = 3; i <= size; i++){
+		mem[i] = max(mem[i-2], mem[i-1]) + v[i];
+>>>>>>> 4163fbf (change directory & start/dp/ sequence.cpp)
 	}
 
 	cout << mem[size];
