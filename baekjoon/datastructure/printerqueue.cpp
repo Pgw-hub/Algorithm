13c3fb4a6fdab7bffc7a4103276039f0dc85912e
parkgeonwoo <80042706+Pgw-hub@users.noreply.github.com>
2022-11-04 09:22:48 +0900

    complete datastructure/printerqueue

--- baekjoon/datastructure/printerqueue.cpp
+++ baekjoon/datastructure/printerqueue.cpp
@@ -13,10 +13,9 @@ int main(){
     cin >> count;
 
     while(count--){
-        int imp_ref[10] = {};
-        int cursor = 9;
         queue<int> order;
         queue<int> imp;
+        priority_queue<int> imp_ref;
         int N, M;
 
         cin >> N >> M;
@@ -26,46 +25,33 @@ int main(){
 
             order.push(i);
             imp.push(tmp);
-            imp_ref[tmp]++;
+            imp_ref.push(tmp);
 
-            if(i == M + 1){
+            if(i == M + 1){//make answer value
                 index_answer = i;
                 imp_answer = tmp;
             }
         }
 
-        //initialize cursor
-        for(int i = 9; i > 0; --i){
-            if(imp_ref[i] == 0) cursor--;
-            else break;
-        }
-
-       //imp_ref에서 가장 오른쪽에 있는 친구가 현재 imp의 front인지 확인.
-       //imp_ref가 0이면 넘기기.
-       //매번 front가 index_answer 인지, imp_answer인지 확인.
         int bunjjae = 1;
-        while(!(index_answer == order.front() && imp_answer == imp.front() && imp.front() == cursor)){
-            //만약 imp.front()가 Imp_ref의 가장 오른쪽에 있는 친구가 같지 않으면 order front와 imp front를 뒤로 보냄, bunjjae ++;
-            //같으면 imp_ref의 0이 아닌 가장 오른쪽 수를 하난 줄임.
-            if(imp_ref[cursor] == 0){
-                cursor--;
+        //till answer comes out
+        while(!(index_answer == order.front() && imp_answer == imp.front() && imp.front() == imp_ref.top())){
+            //violate the imp_order
+            if(imp.front() != imp_ref.top()){
+                order.push(order.front());
+                order.pop();
+                imp.push(imp.front());
+                imp.pop();
             }
+            //ok with imp_order -> print
             else{
-                if(imp.front() != cursor){
-                    order.push(order.front());
-                    order.pop();
-                    imp.push(imp.front());
-                    imp.pop();
-                }
-                else{
-                    order.pop();
-                    imp.pop();
-                    imp_ref[cursor]--;
-                    bunjjae++;
-                }
+                order.pop();
+                imp.pop();
+                imp_ref.pop();
+                bunjjae++;
             }
         }
-        cout << bunjjae;
+        cout << bunjjae << endl;
     }
     return 0;
 }
