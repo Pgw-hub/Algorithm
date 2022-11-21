bef7114dd67f2441464648a6ad342e0f3e50a90e
parkgeonwoo <80042706+Pgw-hub@users.noreply.github.com>
2022-11-21 22:40:05 +0900

    complete basic/qmark.cpp

---
+++ baekjoon/basic/qmark.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	
+	string name;
+	string postfix1 = "??";
+	string postfix2 = "!";
+
+	cin >> name;
+	cout << name + postfix1 + postfix2;
+
+	return 0;
+}
+
