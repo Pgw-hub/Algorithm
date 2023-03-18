efb60971b2015e605286ad8f7ddb0ebcec4a5613
parkgeonwoo <80042706+Pgw-hub@users.noreply.github.com>
2023-03-18 22:03:47 +0900

    practice declaration & definition

---
+++ CPP/add.cpp
@@ -0,0 +1,11 @@
+#include "include/module.h"
+
+int add(int a, int b){
+    int sum = a + b;
+    return sum;
+}
+
+int sub(int a, int b){
+    int sub = a - b;
+    return sub;
+}
---
+++ CPP/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "include/module.h"
+
+using namespace std;
+
+int main(){
+    cout << add(3,5) << endl;
+    cout << sub(5,3) << endl;
+    return 0;
+}
