1ae5c67ea3f315041334c0b732e75f9754c488e1
parkgeonwoo <80042706+Pgw-hub@users.noreply.github.com>
2023-05-18 11:46:45 +0900

    basic/star

---
+++ BAEKJOON/basic/apb.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int N;
+
+	cin >> N;
+	
+	for(int i = 1; i <= N; i++){
+		int a,b = 0;
+
+		cin >> a >> b;
+		cout << "Case #" << i << ":"  << a + b << endl;
+	}
+
+
+
+
+}
